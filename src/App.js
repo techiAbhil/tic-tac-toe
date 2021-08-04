a07f03c390ace51d0c39ec3a427059c279f10900
techiabhil <techiabhil@gmail.com>
2021-08-04 10:05:18 +0530

    css changes, business logic added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+
+const initialBoxState = Array(9).fill(null);
+
+const lineArray = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+
+  [0, 4, 8],
+  [2, 4, 6],
+
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8]
+
+]
+
 
 function App() {
+
+  const [boxState, setBoxState] = useState(initialBoxState);
+  const [tickCount, setTickCount] = useState(0);
+
+  const drawXO = (boxID) => {
+    if (boxState[boxID]) {
+      // if key is not null then return 
+      return;
+    }
+    const updatedBoxState = [...boxState];
+    updatedBoxState[boxID] = tickCount % 2 === 0 ? 'X' : 'O';
+    setBoxState(updatedBoxState);
+    setTickCount(tickCount + 1);
+
+    let winner = null;
+    lineArray.forEach((line) => {
+      if (line.every(columnIndex => updatedBoxState[columnIndex] === 'X')) {
+        winner = 'X';
+      } else if (line.every(columnIndex => updatedBoxState[columnIndex] === 'O')) {
+        winner = 'O';
+      }
+    });
+
+    if (winner)
+      alert(`Winner is ${winner}`);
+  }
+
+  const onClearBtnClick = () => {
+    setBoxState(initialBoxState);
+    setTickCount(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="box-container">
+        {
+          boxState.map((_, index) => {
+            return (
+              <div key={index} id={`box-${index}`} className="box" onClick={() => drawXO(index)}>
+                {boxState[index] ?? ""}
+              </div>
+            )
+          })
+        }
+      </section>
+
+      <div className="btn-container">
+        <button className="btn" disabled={tickCount === 0} onClick={onClearBtnClick}>Clear</button>
+      </div>
+    </main>
   );
 }
 
